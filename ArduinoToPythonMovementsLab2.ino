780db24101f1fab40a8c3c2694e2634831a6f38c
Ian Khoo <ian.t.khoo@gmail.com>
2025-10-28 15:13:28 -0400

    Add files via upload
    This file has the arduino code for recieving and sending signals and data from the arduino to the python script, and in vice versa

---
+++ ArduinoToPythonMovementsLab2.ino
@@ -0,0 +1,51 @@
+// Serial-buttons.ino
+//change the PINS below in accordance to design
+const int btnUp = 2;
+const int btnLeft = 3;
+const int btnDown = 4;
+const int btnRight = 5;
+const int bzrAlert - 6;
+int buzzer_status = LOW;
+int incomingByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btnUp, INPUT_PULLUP);
+  pinMode(btnLeft, INPUT_PULLUP);
+  pinMode(btnDown, INPUT_PULLUP);
+  pinMode(btnRight, INPUT_PULLUP);
+  pinMode(bzrAlert, OUTPUT);
+  digitalWrite(bzrAlert, LOW); 
+}
+
+void loop() {
+  if (digitalRead(btnUp) == LOW) {
+    Serial.println("w");
+    delay(150); 
+  }
+  if (digitalRead(btnLeft) == LOW) {
+    Serial.println("a");
+    delay(150);
+  }
+  if (digitalRead(btnDown) == LOW) {
+    Serial.println("s");
+    delay(150);
+  }
+  if (digitalRead(btnRight) == LOW) {
+    Serial.println("d");
+    delay(150);
+  }
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    Serial.println(incomingByte);
+    
+    if(incomingByte == 'A') {
+      digitalWrite(bzrAlert, HIGH);
+      delay(100);
+      digitalWrite(bzrAlert, LOW);
+    }
+  }
+  
+}
